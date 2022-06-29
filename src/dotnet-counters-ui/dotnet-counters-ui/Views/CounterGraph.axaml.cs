4c1bfe463373b6247229cc758582f3da98d5563b
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2022-06-29 20:08:18 +0200

    Cleaning the code.

--- src/dotnet-counters-ui/dotnet-counters-ui/Views/CounterGraph.axaml.cs
+++ src/dotnet-counters-ui/dotnet-counters-ui/Views/CounterGraph.axaml.cs
@@ -3,29 +3,27 @@ using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using DotnetCountersUi.ViewModels;
 
-
 namespace DotnetCountersUi.Views;
 
-public partial class CounterGraph : ReactiveUserControl<UserControl>
+public class CounterGraph : ReactiveUserControl<UserControl>
 {
   public string GraphId { get; set; } = null!;
 
   public CounterGraph()
   {
     InitializeComponent();
-
   }
-
 
   private void InitializeComponent()
   {
     AvaloniaXamlLoader.Load(this);
   }
-    
+
   protected override void OnInitialized()
   {
-    var dc = (CounterGraphViewModel?)DataContext;
-    dc!.Register(GraphId);
+    var dc = (CounterGraphViewModel?)DataContext!;
+    dc.Register(GraphId);
+    
     var label = this.Find<TextBlock>("Label");
     label.Text = GraphId;
   }
