bf44cde767e9fafb0e238ae0a25c3bea41579446
Piotr Romanowski <wsrh456@gmail.com>
2022-08-17 09:36:07 +0200

    Add GraphToolbox with delete button (#31)

--- src/dotnet-counters-ui/dotnet-counters-ui/Views/CounterGraph.axaml.cs
+++ src/dotnet-counters-ui/dotnet-counters-ui/Views/CounterGraph.axaml.cs
@@ -1,3 +1,4 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
@@ -5,6 +6,15 @@ namespace DotnetCountersUi.Views;
 
 public partial class CounterGraph : UserControl
 {
+    public object Header
+    {
+        get => GetValue(HeaderProperty);
+        set => SetValue(HeaderProperty, value);
+    }
+    
+    public static readonly StyledProperty<object> HeaderProperty = 
+        AvaloniaProperty.Register<CounterGraph, object>(nameof(Header));
+
     public CounterGraph()
     {
         InitializeComponent();
---
+++ src/dotnet-counters-ui/dotnet-counters-ui/Views/GraphToolbox.axaml.cs
@@ -0,0 +1,42 @@
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DotnetCountersUi.Views;
+
+public partial class GraphToolbox : UserControl
+{
+    public ICommand? DeleteCommand
+    {
+        get => _deleteCommand;
+        set => SetAndRaise(DeleteCommandProperty, ref _deleteCommand, value);
+    }
+    
+    public static readonly DirectProperty<GraphToolbox, ICommand?> DeleteCommandProperty =
+        AvaloniaProperty.RegisterDirect<GraphToolbox, ICommand?>(nameof(DeleteCommand),
+            control => control.DeleteCommand, (button, command) => button.DeleteCommand = command, enableDataValidation: true);
+
+    private ICommand? _deleteCommand;
+
+    public object? DeleteCommandParameter
+    {
+        get => _deleteCommandParameter;
+        set => SetAndRaise(DeleteCommandParameterProperty, ref _deleteCommandParameter, value);
+    }
+    
+    public static readonly StyledProperty<object?> DeleteCommandParameterProperty =
+        AvaloniaProperty.Register<GraphToolbox, object?>(nameof(DeleteCommandParameter));
+
+    private object? _deleteCommandParameter;
+
+    public GraphToolbox()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
